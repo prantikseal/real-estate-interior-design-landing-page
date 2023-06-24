9fe5e086043787124221141a1cb6eac640e4b5a5
prantikseal <83081442+prantikseal@users.noreply.github.com>
2023-06-24 22:30:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import NavBar from './components/NavBar.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1>Interior Karo</h1>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
